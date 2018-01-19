48e2a9f0186983a5202db8e8d10d696b92de4cc3
bpelikan <bartoszpelikan@gmail.com>
2018-01-19 19:21:50 +0100

    Add Express.Router

--- Projekt/WebApi/app.js
+++ Projekt/WebApi/app.js
@@ -1,18 +1,30 @@
 // Import the express library here
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const { createElement, getIndexById } = require('./Helpers/helperFunctions');
+
 // Instantiate the app here
 const app = express();
 app.use(bodyParser.json());// to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));// to support URL-encoded bodies
-//app.use(express.urlencoded()); // to support URL-encoded bodies
 
 const PORT = process.env.PORT || 4001;
-//udostepnia pliki z folderu public
-//static/jsonFile/test.json
 app.use('/static', express.static('public'));
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+    // Pass to next layer of middleware
+    next();
+});
 
 const friends = [
     {
@@ -78,23 +90,19 @@ const friends = [
 ]
 
 
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-    // Pass to next layer of middleware
-    next();
-});
+const friendsRouter = express.Router();
+const groupsRouter = express.Router();
+app.use('/friends', friendsRouter);
+app.use('/groups', groupsRouter);
 
-//TODO przed pobraniem sprawdzic czy istnije podane id
-app.get("/friends/:id", (req, res, next) => {
+
+
+friendsRouter.get("/", (req, res, next) => {
+    console.log('GET /friends');
+    res.json(friends);
+})
+
+friendsRouter.get("/:id", (req, res, next) => {
     console.log('/friend/' + req.params.id);
     const friendIndex = getIndexById(req.params.id, friends);
     
@@ -105,13 +113,45 @@ app.get("/friends/:id", (req, res, next) => {
     }
 })
 
-app.get("/friends", (req, res, next) => {
-    console.log('GET /friends');
+friendsRouter.put('/:id', (req, res, next) => {
+    console.log('PUT /friends/' + req.params.id);
+    let friendUpdate = req.body;
+    const friendIndex = getIndexById(req.params.id, friends);
 
-    res.json(friends);
+    if (friendIndex !== -1) {
+        friendUpdate.id = friendIndex;
+        friends[friendIndex] = friendUpdate;
+        res.json(friends[friendIndex]);
+    } else {
+        res.status(404).send();
+    }
 })
 
-app.get("/groups", (req, res, next) => {
+friendsRouter.post('/', (req, res, next) => {
+    console.log('POST /friends');
+    const receivedFriend = createElement('friends', req.body);
+    
+    if (receivedFriend) {
+        friends.push(receivedFriend);
+        res.status(201).json(receivedFriend);
+      } else {
+        res.status(400).send();
+      }
+})
+
+friendsRouter.delete('/:id', (req, res, next) => {
+    console.log('DELETE /friends/' + req.params.id);
+    const friendIndex = getIndexById(req.params.id, friends);
+    
+    if (friendIndex !== -1) {
+        friends.splice(friendIndex,1);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+})
+
+groupsRouter.get("/", (req, res, next) => {
     console.log('GET /groups');
 
     let groupsArray = [];
@@ -126,44 +166,11 @@ app.get("/groups", (req, res, next) => {
 })
 
 
-//PUT /friends/0?id=0&firstName=Jacek&lastName=Jackowiak&group=Grupa1
-app.put('/friends/:id', (req, res, next) => {
-    console.log('PUT /friends/' + req.params.id);
-    let friendUpdate = req.body;
-    const friendIndex = getIndexById(req.params.id, friends);
 
-    if (friendIndex !== -1) {
-        friendUpdate.id = friendIndex;
-        friends[friendIndex] = friendUpdate;
-        res.json(friends[friendIndex]);
-    } else {
-        res.status(404).send();
-    }
-})
 
-app.post('/friends', (req, res, next) => {
-    console.log('POST /friends');
-    const receivedFriend = createElement('friends', req.body);
-    
-    if (receivedFriend) {
-        friends.push(receivedFriend);
-        res.status(201).json(receivedFriend);
-      } else {
-        res.status(400).send();
-      }
-})
 
-app.delete('/friends/:id', (req, res, next) => {
-    console.log('DELETE /friends/' + req.params.id);
-    const friendIndex = getIndexById(req.params.id, friends);
-    
-    if (friendIndex !== -1) {
-        friends.splice(friendIndex,1);
-        res.status(204).send();
-    } else {
-        res.status(404).send();
-    }
-})
+
+
 
 // Invoke the app's `.listen()` method below:
 app.listen(PORT, () => {
