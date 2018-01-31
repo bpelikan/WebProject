e0f460883ed20fc58821701d403e8e28a798de5e
bpelikan <bartoszpelikan@gmail.com>
2018-01-31 21:21:41 +0100

    Delete unused friends array

--- Projekt/WebApi/Routers/friendsRouter.js
+++ Projekt/WebApi/Routers/friendsRouter.js
@@ -44,69 +44,6 @@ var Friend = mongoose.model('Friend', friendSchema);
 const friendsRouter = express.Router();
 module.exports = friendsRouter;
 
-const friends = [
-    {
-        id: 0,
-        firstName: "Adam",
-        lastName: "Misiewicz",
-        phoneNumber: "123456789",
-        email:"amisiewicz@gmail.com",
-        street: "ul. Mleczna",
-        number: "1",
-        postalCode: "00-101",
-        city: "Warszawa",
-        group: "Rodzina"
-    },
-    {
-        id: 1,
-        firstName: "Alicja",
-        lastName: "Gosiak",
-        phoneNumber: "234456567",
-        email:"agosiak@gmail.com",
-        street: "ul. Słoneczna",
-        number: "2",
-        postalCode: "00-201",
-        city: "Kraków",
-        group: "Praca"
-    },
-    {
-        id: 2,
-        firstName: "Anna",
-        lastName: "Isiewicz",
-        phoneNumber: "123456789",
-        email:"aisiewicz@gmail.com",
-        street: "ul. Kolejowa",
-        number: "3",
-        postalCode: "00-301",
-        city: "Poznań",
-        group: "Inne"
-    },
-    {
-        id: 3,
-        firstName: "Adam",
-        lastName: "Misiewicz",
-        phoneNumber: "123456789",
-        email:"amisiewicz2@gmail.com",
-        street: "ul. Stołowa",
-        number: "4",
-        postalCode: "00-401",
-        city: "Gdynia",
-        group: "Test"
-    },
-    {
-        id: 4,
-        firstName: "Jacek",
-        lastName: "Misiewicz",
-        phoneNumber: "123456789",
-        email:"jmisiewicz@gmail.com",
-        street: "ul. Stołowa",
-        number: "5",
-        postalCode: "00-501",
-        city: "Sopot",
-        group: "Grupa1"
-    }
-];
-
 friendsRouter.get("/", (req, res, next) => {
     Friend.find().exec(function(err, friends) {
         if (err) {
