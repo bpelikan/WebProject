0dbe4a51fdf6c8baa20ea8906012c55dafe7da0b
bpelikan <bartoszpelikan@gmail.com>
2018-02-02 02:57:02 +0100

    Displaying friend details

--- Projekt/Web/js/controllers/DetailsController.js
+++ Projekt/Web/js/controllers/DetailsController.js
@@ -1,8 +1,8 @@
 app.controller('DetailsController', ['$scope', 'friendDetails', '$routeParams', function($scope, friendDetails, $routeParams) { 
     console.log("DetailsController");
     friendDetails.then(function(data) {
-        $scope.friendDetails = data;
+        $scope.friend = data;
     });
 
-    
+
 }]);
