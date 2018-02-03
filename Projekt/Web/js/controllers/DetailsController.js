6a2b40ce8565630d9eb6bc49846b2e0803b3700e
bpelikan <bartoszpelikan@gmail.com>
2018-02-03 18:04:11 +0100

    Move deleting friend to DetailsController

--- Projekt/Web/js/controllers/DetailsController.js
+++ Projekt/Web/js/controllers/DetailsController.js
@@ -1,15 +1,21 @@
-app.controller('DetailsController', ['$scope', 'friendDetails', 'groups', '$window', function($scope, friendDetails, groups, $window) { 
+app.controller('DetailsController', ['$scope', 'friendDetails', 'groups', 'deleteFriendService', '$window', '$route', function($scope, friendDetails, groups, deleteFriendService, $window, $route) { 
     console.log("DetailsController");
 
     groups.showGroups().then(function(data) {
-        //console.log(data);
         $scope.groups = data;
     });
 
     friendDetails.showDetails().then(function(data) {
-        //console.log(data);
         $scope.friend = data;
     });
+
+    this.deleteFriend = function() {
+        deleteFriendService.deleteFriend().then(function(data) {
+            //console.log(data);
+            $window.history.back();
+            $route.reload();
+        });
+    }
 
     this.back = function(){
         console.log("back");
--- Projekt/Web/js/services/deleteFriend.js
+++ Projekt/Web/js/services/deleteFriend.js
@@ -1,14 +1,12 @@
-app.service('deleteFriendService', ['$http', '$route', '$routeParams', function($http, $route, $routeParams) { 
-    this.deleteFriend = function(friendToEdit){
+app.service('deleteFriendService', ['$http', '$routeParams', function($http, $routeParams) { 
+    this.deleteFriend = function(){
         return $http({
             method: 'DELETE',
             headers: {'Content-Type': 'application/json'},
-            url: 'http://localhost:4001/friends/' + $routeParams.friendId,
-            data: friendToEdit
+            url: 'http://localhost:4001/friends/' + $routeParams.friendId
         }).then(function (success){
             console.log("Delete friend service");
             console.log(success);
-            $route.reload();
             return success.data;
         },function (error){
             console.log("Error during saving friends from api");
